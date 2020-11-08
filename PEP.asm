1a603675f0a7745bc9d3cbe2ee9c2468a91976f4
Mario <mariolaramolina21@gmail.ciom>
2020-11-07 20:41:00 -0600

    Inicialización del trabajo

---
+++ PEP.asm
@@ -0,0 +1,11 @@
+;-------------------------------------------;
+;Creado por: Mario Lara Molina              ;
+;Fecha de creación: 07/11/2020              ;
+;Última modificación: 07/11/2020            ;
+;Forma de compilación:                      ;
+; Debe ingresar en la línea de comando      ;
+;  Usando el turbo assembler de borland 4:  ;
+;          compilar PEP                     ;
+;  Y si desea solo ejecutarlo:              ;
+;          PEP                              ;
+;-------------------------------------------;
